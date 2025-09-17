4d88aa4c7d7d1a6ac4f562f37f6212cbb202ade4
IkanBerkepalaLele <ehgojim@gmail.com>
2025-09-17 16:32:50 +0700

    tambah login logout register

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
     CATEGORY_CHOICES = [('shoe', 'Shoe'), ('jersey', 'Jersey'), ('accessory', 'Accessory'), ('shorts', 'Shorts')]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
